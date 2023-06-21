5a1386f6eef3c5c4c9c75cdc98c6cbc6183f06a5
Jiradett Kerdsri <jiradett@gmail.com>
2023-06-22 09:47:30 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
 
-streamlit.title('Title')
-streamlit.header('Header')
-streamlit.text('Text')
+ 
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
